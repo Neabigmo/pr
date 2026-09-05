c7d57e8eda795b3f30db82fb8f1da46e7f17c68b
杨一横 <3025660180@qq.com>
2026-09-05 11:36:24 +0800

    code: initialize pr_pilot package

---
+++ src/pr_pilot/__init__.py
@@ -0,0 +1,7 @@
+"""PR mini-pilot package.
+
+The package is deliberately organized so that data freezing, model definition,
+training stages, baseline wrappers and evaluation can be audited independently.
+"""
+
+__version__ = "0.1.0"
